44ae97b348f8c7e51781dd05b293abd22fddf7f6
Pertixx <pertix2000@gmail.com>
2022-04-06 00:37:40 -0300

    Added custom fonts

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
+import AppLoading from 'expo-app-loading';
 import Home from './screens/Home';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    Heebo: require('./assets/fonts/Heebo-Regular.ttf'),
+    HeeboBold: require('./assets/fonts/Heebo-Bold.ttf'),
+  })
+
+  if (!loaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <Home />
-    </View>
+    </SafeAreaView>
   );
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text style={{fontFamily: 'HeeboBold'}}>Home</Text>
     </View>
   )
 }
